ec3ee1a50476fb7ae64401c3e9f4d0ebba77f5cc
shashank <saggarwal617@gmail.com>
2018-05-21 01:34:27 +0530

    INdexofExample

---
+++ IndexOfExample.java
@@ -0,0 +1,9 @@
+public class IndexOfExample {
+    public static void main(String args[]) {
+        String s1 = "Don't Know but I like to stay awake at night";
+        int index1 = s1.indexOf("like");
+        int index12 = s1.indexOf("Like");
+        int index3 = s1.indexOf("q");
+        System.out.println(index1 + " " + index12  + "" + index3 );
+    }
+ }
