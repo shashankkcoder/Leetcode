620d7459e6b6b51fc5e3d734afcbeadfe0915ea8
shashank <saggarwal617@gmail.com>
2018-05-20 11:28:44 +0530

    LengthOFLaStword

---
+++ LengthOfWord.java
@@ -0,0 +1,10 @@
+// class Solution {
+//     public int lengthOfLastWord(String string) {
+        
+//        if (string.trim().length() <=0 ) {   return 0; }
+       
+//         int lengthOfString = string.length();
+//         String[] splitter = string.split(" ");
+//         return ((splitter[splitter.length-1]).length());
+//     }
+// }
